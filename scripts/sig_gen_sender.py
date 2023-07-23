b65cb6031f9f0cdbae852529f16ebc59d6e153c1
Electro-resonance <martin.timms@electrozest.co.uk>
2023-07-23 21:55:43 +0100

    Make network script more flexible
    Change to a class so that the script become reusable. Give options to
    set amplitude,phase and frequency every iteration, or only if there is a
    change in value.

--- scripts/sig_gen_sender.py
+++ scripts/sig_gen_sender.py
@@ -20,83 +20,123 @@
 #  Note that the remote end forwards on frequency commands to the signal generators it 
 #  has detected and automatically igniores commands to signal generators not yet connected.
 
+#!/usr/bin/python3
 
 import socket
 import configparser
 import time
 
-def read_config():
-    config = configparser.ConfigParser()
-    config.read('sig_gen_config.ini')
-    ip_address = config['Server']['IP']
-    port = int(config['Server']['Port'])
-    frequencies = config['Frequencies']['Frequencies'].replace(' ', '').split(',')
-    interval = int(config['SignalGeneratorSetup']['Interval'])  # Get the interval in seconds
-    init_commands = config['SignalGeneratorSetup']['InitCmds'].replace('"', '')
-    extra_commands = {}
-    for key, value in config['SignalGeneratorSetup'].items():
-        if key.startswith('cmd'):
-            extra_commands[key] = value.replace('"', '')
-    return ip_address, port, frequencies, interval, init_commands, extra_commands
+class SignalGeneratorNetworkSender:
+    def __init__(self):
+        self.ip_address = ""
+        self.port = 0
+        self.frequencies = []
+        self.phases = []
+        self.amplitudes = []
+        self.prev_frequencies = []
+        self.prev_phases = []
+        self.prev_amplitudes = []
+        self.interval = 0
+        self.init_commands = ""
+        self.extra_commands = {}
+        self.sock = None
+        self.init_sent_time = 0
+        self.read_config()
 
+    def read_config(self):
+        config = configparser.ConfigParser()
+        config.read('sig_gen_config.ini')
+        self.ip_address = config['Server']['IP']
+        self.port = int(config['Server']['Port'])
+        self.frequencies = config['Signals']['Frequencies'].replace(' ', '').split(',')
+        self.phases = config['Signals']['Phases'].replace(' ', '').split(',')
+        self.amplitudes = config['Signals']['Amplitudes'].replace(' ', '').split(',')
+        self.interval = int(config['SignalGeneratorSetup']['Interval'])  # Get the interval in seconds
+        self.init_commands = config['SignalGeneratorSetup']['InitCmds'].replace('"', '')
+        for key, value in config['SignalGeneratorSetup'].items():
+            if key.startswith('cmd'):
+                self.extra_commands[key] = value.replace('"', '')
+        for index in range(len(self.frequencies)):
+            self.prev_frequencies.append(-1)
+            self.prev_phases.append(-1)
+            self.prev_amplitudes.append(-1)
 
-def send_command(sock, command):
-    sock.sendall(command.encode())
-    print("sending command:",command.replace('\n',''))
+    def send_command(self, command):
+        self.sock.sendall(command.encode())
+        print("sending command:", command.replace('\n', ''))
 
-def send_init(sock,frequencies,init_commands,extra_commands):
-    # Send the initial commands for each signal generator
-    print("*** Sending sig gen initialiser ***")
-    for index in range(len(frequencies)):
-        lines = init_commands.split(",")
-        for line in lines:
-            line_cmd = f"{index:02}{line}\n"
-            send_command(sock, line_cmd)
-    print("*** Sending extra commands ***")
-    for key, value in extra_commands.items():
-        index = int(key[3:])  # Extract the address from the key (cmd00, cmd01, ...)
-        lines = value.split(",")
-        for line in lines:
-            line_cmd = f"{index:02}{line}\n"
-            send_command(sock, line_cmd)
-    print("*** Completed ***")
+    def send_init(self):
+        # Send the initial commands for each signal generator
+        print("*** Sending signal generator initialiser ***")
+        for index in range(len(self.frequencies)):
+            lines = self.init_commands.split(",")
+            for line in lines:
+                line_cmd = f"{index:02}{line}\n"
+                self.send_command(line_cmd)
 
-def main():
-    ip_address, port, frequencies, interval, init_commands, extra_commands = read_config()
-    init_sent_time = 0
+    def send_extra_commands(self):
+        # Send the extra commands for each signal generator
+        print("*** Sending extra commands ***")
+        for key, value in self.extra_commands.items():
+            index = int(key[3:])  # Extract the address from the key (cmd00, cmd01, ...)
+            lines = value.split(",")
+            for line in lines:
+                line_cmd = f"{index:02}{line}\n"
+                self.send_command(line_cmd)
 
+    def send_freq_phase_amplitudes(self,set_all=True):
+        for index in range(len(self.frequencies)):
+            # Set the frequency for each signal generator
+            if(set_all or self.frequencies[index]!=self.prev_frequencies[index]):
+                frequency_cmd = f"{index:02}f{self.frequencies[index]}\n"
+                self.send_command(frequency_cmd)
+                self.prev_frequencies[index]=self.frequencies[index]
+            if(set_all or self.phases[index]!=self.prev_phases[index]):
+                phase_cmd = f"{index:02}p{self.phases[index]}\n"
+                self.send_command(phase_cmd)
+                self.prev_phases[index]=self.phases[index]
+            if(set_all or self.amplitudes[index]!=self.prev_amplitudes[index]):         
+                amplitude_cmd = f"{index:02}a{self.amplitudes[index]}\n"
+                self.send_command(amplitude_cmd)
+                self.prev_amplitudes[index]=self.amplitudes[index]
 
-    while True:
-        # Create a TCP/IP socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
+    def set_signal(self,channel,freq,phase,amplitude):
+        self.frequencies[channel]=freq
+        self.phases[channel]=phase
+        self.amplitudes[channel]=amplitude
+
+    def connect(self):
+        if(self.sock==None):
+            # Create a TCP/IP socket
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             # Connect to the server
-            sock.connect((ip_address, port))
-            print(f"Connected to {ip_address}:{port}")
-            while(True):
-                current_time = time.time()
-                elapsed_time = current_time - init_sent_time
-                if elapsed_time > interval:
-                    #Resend the signal generator initialiser
-                    send_init(sock,frequencies,init_commands,extra_commands)
-                    init_sent_time = time.time()
+            self.sock.connect((self.ip_address, self.port))
+            print(f"Connected to {self.ip_address}:{self.port}")
 
-                for index in range(len(frequencies)):    
-                    # Set the frequency for each signal generator
-                    frequency_cmd = f"{index:02}f{frequencies[index]}\n"
-                    send_command(sock, frequency_cmd)
-
-                time.sleep(1)
-
+    def run(self,set_all=True):
+        try:
+            self.connect()
+            current_time = time.time()
+            elapsed_time = current_time - self.init_sent_time
+            if elapsed_time > self.interval:
+                # Send the initial commands and extra commands
+                self.send_init()
+                self.send_extra_commands()
+                print("*** Initialised ***")
+                self.init_sent_time = current_time
+            self.send_freq_phase_amplitudes(set_all)
         except Exception as e:
             print(f"Error: {e}")
-            print("Reconnecting after a delay...")
-            time.sleep(2) 
-        finally:
-            # Close the socket before reconnecting or terminating the loop.
-            sock.close()
-            print("Socket closed.")
-            time.sleep(2) 
+            self.sock.close()
+            self.sock=None
+            return False
+        return True;
+
 
 if __name__ == "__main__":
-    main()
+    sender = SignalGeneratorNetworkSender()
+    while(True):
+        if(sender.run(set_all=True)==False):
+            print("Reconnecting after a delay...")
+            time.sleep(2)
+        time.sleep(1)
